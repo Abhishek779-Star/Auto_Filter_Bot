cde9a7c757a9e61297c8781d48de348d59720e38
Abhishek779-Star <abhishekbro779@gmail.com>
2025-04-05 19:15:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,7 +50,7 @@ DATABASE_URI2 = environ.get('DATABASE_URI2', "")
 
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Movies_World_Request_Group_hdx')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/MOVIES_WORLDZS')
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/')
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/AV_King1')
 UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/MOVIES_WORLDZS')
 
 #Force Subscription Channel (Put Same Channel Id In Both Veriables)
