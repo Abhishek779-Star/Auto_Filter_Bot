90df6a2fda71a44225070eea000136cf4bd5e58e
Mimamofficial <mrmuzaffarimam@gmail.com>
2025-05-20 18:04:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -121,12 +121,12 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else "https://{}/".format(FQDN, PORT)
+URL = "http://jealous-tonya-123456789123456789123456789123456789pro-f8f0bafe.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else "http://jealous-tonya-123456789123456789123456789123456789pro-f8f0bafe.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
-SESSION_NAME = str(environ.get('SESSION_NAME', 'SilentXBotz'))
+SESSION_NAME = str(environ.get('SESSION_NAME', 'MRN_RIPPER'))
 MULTI_CLIENT = False
-name = str(environ.get('name', 'SilentX'))
+name = str(environ.get('name', 'MRN_RIPPER'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if 'DYNO' in environ:
     ON_HEROKU = True
