df2e2b8aa96072cc95b8d1f2314484fffb922481
Abhishek779-Star <abhishekbro779@gmail.com>
2026-07-02 15:25:17 +0530

    Update shortener API and website URLs

--- info.py
+++ info.py
@@ -67,14 +67,14 @@ TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/Howtoverifyanylink")
 TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/Howtoverifyanylink")
 
 # Verification (Must Fill All Veriables. Else You Got Error
-SHORTENER_API = environ.get("SHORTENER_API", "c6be39f9ee99048543b5c20e012f2336a5be3dfe")
-SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "papajiurl.com")
+SHORTENER_API = environ.get("SHORTENER_API", "99efe0795dc613a7a0a67f5d8c388f41efacac4f")
+SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "arolinks.com")
 
-SHORTENER_API2 = environ.get("SHORTENER_API2", "c6be39f9ee99048543b5c20e012f2336a5be3dfe")
-SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "papajiurl.com")
+SHORTENER_API2 = environ.get("SHORTENER_API2", "99efe0795dc613a7a0a67f5d8c388f41efacac4f")
+SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "arolinks.com")
 
-SHORTENER_API3 = environ.get("SHORTENER_API3", "c6be39f9ee99048543b5c20e012f2336a5be3dfe")
-SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", "papajiurl.com")
+SHORTENER_API3 = environ.get("SHORTENER_API3", "99efe0795dc613a7a0a67f5d8c388f41efacac4f")
+SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", "arolinks.com")
 
 TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "86400"))
 THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "43200"))
